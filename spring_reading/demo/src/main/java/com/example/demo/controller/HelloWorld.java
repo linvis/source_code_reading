0d2bf043397a626575d817195b98aa22a95e3696
shlinym <jarvisfironman@gmail.com>
2020-12-05 15:32:15 +0800

    add tomcat

---
+++ spring_reading/demo/src/main/java/com/example/demo/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/hello/world")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
